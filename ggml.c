30edb4852e156f9fca5df60def85f11ba6ad9abc
mingfeima <mingfei.ma@intel.com>
2024-04-06 19:57:25 -0700

    add intel amx isa detection

--- ggml.c
+++ ggml.c
@@ -21676,6 +21676,14 @@ int ggml_cpu_has_avx512_vnni(void) {
 #endif
 }
 
+int ggml_cpu_has_amx_int8(void) {
+#if defined(__AMX_INT8__)
+    return 1;
+#else
+    return 0;
+#endif
+}
+
 int ggml_cpu_has_fma(void) {
 #if defined(__FMA__)
     return 1;
